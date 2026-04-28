59081577a99b47c6d8e68f19cd8dc88557e991c7
sgrinley <shamar808@yahoo.com>
2026-04-28 07:46:03 -0700

    Initial commit

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Transaction {
+
+    //  Step 1: Establish Data fields (instance variables)for each transaction
+    //  date|time|description|vendor|amount
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+
 }

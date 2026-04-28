ad887648ed8f038aedf9425c067ed84b2856ae06
sgrinley <shamar808@yahoo.com>
2026-04-28 13:12:29 -0700

    feat: create Ledger Screen

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -20,13 +20,46 @@ public class App {
         boolean isRunning = true;
 
         while (isRunning) {
-            System.out.println("""
+            System.out.print("""
                     \n--- HOME SCREEN ---
                     D) Add Deposit (Credit +)
                     P) Make Payment (Debit -)
                     L) Ledger
                     X) Exit
                     Enter choice""");
+            String choice = scanner.nextLine().toLowerCase().trim();
+
+            switch (choice) {
+                case "D" -> System.out.println("Add Deposit");
+                case "P" -> System.out.println("Make Payment");
+                case "L" -> runLedgerScreen();
+                case "X" -> isRunning = false;
+                default -> System.out.println("Invalid input. Try again!");
+            }
+        }
+    }
+    //  Create: LEDGER SCREEN
+    private static void runLedgerScreen() {
+        boolean inLedger = true;
+
+        while (inLedger) {
+            System.out.print("""
+                    \n--- LEDGER SCREEN ---
+                    A) All Entries
+                    D) Deposits
+                    P) Payments
+                    R) Reports
+                    R) Home
+                    Enter choice""");
+            String choice = scanner.nextLine().toLowerCase().trim();
+
+            switch (choice) {
+                case "D" -> System.out.println("Add Deposit");
+                case "P" -> System.out.println("Make Payment");
+                case "L" -> runLedgerScreen();
+                case "X" -> inLedger = false;
+                default -> System.out.println("Invalid input. Try again!");
+            }
         }
     }
 }

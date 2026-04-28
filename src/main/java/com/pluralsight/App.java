62e449dad30d7eb91fad917018dacaa1171a8efe
sgrinley <shamar808@yahoo.com>
2026-04-28 09:23:56 -0700

    feat: Import ArrayList to store data & Scanner to read input data

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    //  Establish Arraylist
+    static ArrayList<Transaction> transactions = new ArrayList<>();
+
+    //  Establish Scanner
+    static Scanner scanner = new Scanner(System.in);
+
+    // =========================
+    // MAIN METHOD (ENTRY POINT)
+    // =========================
+    public static void main(String[] args) {
+
+    }
+
+
 }

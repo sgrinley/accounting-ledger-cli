ef66e601274422d106fea1be64a13186a7630085
sgrinley <shamar808@yahoo.com>
2026-04-28 08:03:53 -0700

    refactor: Rename date/time fields to LocalDate and LocalTime for clarity

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,17 +1,20 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transaction {
 
     //  Step 1: Establish Data fields (instance variables)for each transaction
     //  date|time|description|vendor|amount
-    private String date;
-    private String time;
+    private LocalDate date;
+    private LocalTime time;
     private String description;
     private String vendor;
     private double amount;
 
     //  Step 2: Generate constructor (for transaction object)
-    public Transaction(String date, String time, String description, String vendor, double amount) {
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
         this.date = date;
         this.time = time;
         this.description = description;
